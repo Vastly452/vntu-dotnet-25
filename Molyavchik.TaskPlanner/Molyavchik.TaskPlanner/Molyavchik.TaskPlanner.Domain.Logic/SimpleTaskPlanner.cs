1727ba72f4b3ea21c57714a7b77eb6e470f047ee
Vastly452 <super.exot1k@ukr.net>
2025-09-30 12:30:10 +0300

    new commit

---
+++ Molyavchik.TaskPlanner/Molyavchik.TaskPlanner/Molyavchik.TaskPlanner.Domain.Logic/SimpleTaskPlanner.cs
@@ -0,0 +1,46 @@
+﻿using Molyavchik.TaskPlanner.Domain.Models;
+using Molyavchik.TaskPlanner.DataAccess.Abstractions;
+
+namespace Molyavchik.TaskPlanner.Domain.Logic
+{
+    public class SimpleTaskPlanner
+    {
+        private readonly IWorkItemRepository _repository;
+
+        public SimpleTaskPlanner(IWorkItemRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public WorkItem[] CreatePlan()
+        {
+            var items = _repository
+                .GetAll()
+                .Where(wi => !wi.IsCompleted)
+                .ToList();
+
+            items.Sort(CompareWorkItems);
+            return items.ToArray();
+        }
+
+        private static int CompareWorkItems(WorkItem firstItem, WorkItem secondItem)
+        {
+            // Сортування за Priority (спадання)
+            var priorityComparison = secondItem.Priority.CompareTo(firstItem.Priority);
+            if (priorityComparison != 0)
+            {
+                return priorityComparison;
+            }
+
+            // Сортування за DueDate (зростання)
+            var dateComparison = firstItem.DueDate.CompareTo(secondItem.DueDate);
+            if (dateComparison != 0)
+            {
+                return dateComparison;
+            }
+
+            // Сортування за Title (алфавітний порядок)
+            return string.Compare(firstItem.Title, secondItem.Title);
+        }
+    }
+}
